fed8d3ae8bae1b238fb1b10a1294cfba693fc28b
VoHeLi <106915605+VoHeLi@users.noreply.github.com>
2023-04-22 20:43:30 +0200

    Initial Upload

---
+++ Easy3rdPerson.cs
@@ -0,0 +1,318 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+
+public class Easy3rdPerson : UdonSharpBehaviour
+{
+
+    [Header("-----Internal, don't touch-----")]
+
+    [SerializeField] private RectTransform displayImageTransform;
+    [SerializeField] private RawImage displayImage;
+    [SerializeField] private RawImage displayImageUI;
+    [SerializeField] private RawImage displayImagePlayer;
+
+    [SerializeField] private Camera thirdPersonCamera;
+    [SerializeField] private Camera thirdPersonCameraPlayer;
+    [SerializeField] private Camera firstPersonCamera;
+
+    [SerializeField] private Transform backSpot;
+    [SerializeField] private Transform frontSpot;
+
+    [SerializeField] private GameObject loadingWarning;
+
+    [Header("-----Settings-----")]
+
+    [SerializeField] private KeyCode switchViewInput = KeyCode.X;
+
+
+    [Header("-----Zoom Settings-----")]
+
+    [SerializeField] private float zoomSpeed = 3.0f;
+    [SerializeField] private float zoomMin = 1.0f;
+    [SerializeField] private float zoomMax = 5.0f;
+
+    [Header("-----Center Translate Settings-----")]
+
+    [SerializeField] private KeyCode translateCenterInput = KeyCode.LeftControl;
+    [SerializeField] private float deltaCenterSpeed = 1.0f;
+    [SerializeField] private float maxDeltaCenter = 0.0f;
+    [SerializeField] private float minDeltaCenter = -1.0f;
+
+    [Header("-----Clip Settings-----")]
+
+    [SerializeField] private LayerMask clipLayers;
+    [SerializeField] private bool enableDeltaClip;
+    [SerializeField] private float deltaClip = 0.1f;
+
+    [Header("-----Player Fade-----")]
+
+    [SerializeField] private bool enableGradientPlayer;
+    [SerializeField] private float minimumPlayerDisplayDistance = 1.0f;
+    [SerializeField] private float totalPlayerDisplayDistance = 2.0f;
+
+
+    private RenderTexture firstPersonRenderTexture;
+    private RenderTexture thirdPersonRenderTexture;
+    private RenderTexture thirdPersonRenderTexturePlayer;
+    private Vector2 displaySize;
+
+    private int currentState; //0 = first, 1 = back, 2 = front
+    private float currentFrontScroll = 1;
+    private float currentBackScroll = 1;
+    private float currentDeltaCenter = 0;
+
+
+    private void Start()
+    {
+        if (Networking.LocalPlayer.IsUserInVR())
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        currentState = 1;
+        currentFrontScroll = (zoomMin + zoomMax) / 2;
+        currentBackScroll = (zoomMin + zoomMax) / 2;
+
+        UpdateRenderTexture();
+    }
+
+    private void UpdateRenderTexture()
+    {
+        Vector2 currentDisplaySize = new Vector2(displayImageTransform.rect.width, displayImageTransform.rect.height);
+
+        if (currentDisplaySize.x == 0 && currentDisplaySize.y == 0) return;
+
+        if (currentDisplaySize == displaySize && firstPersonRenderTexture != null && thirdPersonRenderTexture != null && thirdPersonRenderTexturePlayer != null) return;
+
+        displaySize = currentDisplaySize;
+
+        loadingWarning.SetActive(false);
+
+        UpdateThirdRenderTexture();
+        UpdateFirstRenderTexture();
+       
+
+        if (!enableGradientPlayer) return;
+
+        UpdateThirdRenderTexturePlayer();
+    }
+
+    private void UpdateThirdRenderTexture()
+    {
+        if (thirdPersonRenderTexture != null)
+        {
+            thirdPersonRenderTexture.Release();
+        }
+        thirdPersonRenderTexture = new RenderTexture((int)displaySize.x, (int)displaySize.y, 32);
+        thirdPersonRenderTexture.anisoLevel = 8;
+        thirdPersonRenderTexture.autoGenerateMips = true;
+        thirdPersonRenderTexture.antiAliasing = 8;
+
+        thirdPersonCamera.targetTexture = thirdPersonRenderTexture;
+        thirdPersonCamera.enabled = true;
+
+        displayImage.texture = thirdPersonRenderTexture;
+    }
+    private void UpdateFirstRenderTexture()
+    {
+        if (firstPersonRenderTexture != null)
+        {
+            firstPersonRenderTexture.Release();
+        }
+        firstPersonRenderTexture = new RenderTexture((int)displaySize.x, (int)displaySize.y, 32);
+        firstPersonRenderTexture.anisoLevel = 8;
+        firstPersonRenderTexture.autoGenerateMips = true;
+        firstPersonRenderTexture.antiAliasing = 8;
+
+        firstPersonCamera.targetTexture = firstPersonRenderTexture;
+        firstPersonCamera.enabled = true;
+
+        displayImageUI.texture = firstPersonRenderTexture;
+    }
+
+    private void UpdateThirdRenderTexturePlayer()
+    {
+        if (thirdPersonRenderTexturePlayer != null)
+        {
+            thirdPersonRenderTexturePlayer.Release();
+        }
+        thirdPersonRenderTexturePlayer = new RenderTexture((int)displaySize.x, (int)displaySize.y, 32);
+        thirdPersonRenderTexturePlayer.anisoLevel = 8;
+        thirdPersonRenderTexturePlayer.autoGenerateMips = true;
+        thirdPersonRenderTexturePlayer.antiAliasing = 8;
+
+        thirdPersonCameraPlayer.targetTexture = thirdPersonRenderTexturePlayer;
+        thirdPersonCameraPlayer.enabled = true;
+
+        displayImagePlayer.texture = thirdPersonRenderTexturePlayer;
+    }
+
+
+    private void Update()
+    {
+        VRCPlayerApi.TrackingData trackingData = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+        transform.position = trackingData.position + currentDeltaCenter*Vector3.up;
+        firstPersonCamera.transform.localPosition = currentDeltaCenter * Vector3.down;
+        transform.rotation = trackingData.rotation;
+
+        UpdateRenderTexture();
+
+
+        if (Input.GetKeyDown(switchViewInput))
+        {
+            SwitchState();
+        }
+
+
+        UpdateScroll();
+    }
+
+    private void UpdateScroll()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(translateCenterInput))
+        {
+            currentDeltaCenter += deltaCenterSpeed * scroll;
+            currentDeltaCenter = Mathf.Clamp(currentDeltaCenter, minDeltaCenter, maxDeltaCenter);
+            scroll = 0;
+        }
+
+        switch (currentState)
+        {
+            case 1:
+                UpdateScrollBack(scroll);
+                break;
+            case 2:
+                UpdateScrollFront(scroll);
+                break;
+        }
+    }
+
+    private void UpdateScrollBack(float scroll)
+    {
+        currentBackScroll -= scroll * zoomSpeed;
+        currentBackScroll = Mathf.Clamp(currentBackScroll, zoomMin, zoomMax);
+        backSpot.transform.localPosition = currentBackScroll * Vector3.back;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, backSpot.transform.position - transform.position, out hit, currentBackScroll + deltaClip, clipLayers, QueryTriggerInteraction.Ignore))
+        {
+            if ((hit.point - transform.position).sqrMagnitude > zoomMin * zoomMin)
+            {
+                backSpot.transform.position = hit.point;
+            }
+            else
+            {
+                backSpot.transform.localPosition = zoomMin * Vector3.back;
+            }
+            backSpot.transform.localPosition -= backSpot.transform.localPosition.normalized * deltaClip * (1.0f - Vector3.Dot(hit.normal, backSpot.transform.localPosition.normalized));
+
+        }
+
+        //Gradient
+
+        float visibility = Mathf.Clamp01((backSpot.transform.localPosition.magnitude - minimumPlayerDisplayDistance) / (totalPlayerDisplayDistance - minimumPlayerDisplayDistance));
+
+        if (visibility < 1 && enableGradientPlayer)
+        {
+            thirdPersonCamera.cullingMask &= ~((1 << 18) + (1 << 9) + (1 << 10)); //Disable the layer
+            thirdPersonCameraPlayer.gameObject.SetActive(true);
+            displayImagePlayer.color = new Color(1, 1, 1, visibility);
+        }
+        else
+        {
+            thirdPersonCamera.cullingMask |= ((1 << 18) + (1 << 9) + (1 << 10)); //Enable the layer
+            displayImagePlayer.color = new Color(1, 1, 1, 0);
+        }
+
+    }
+
+    private void UpdateScrollFront(float scroll)
+    {
+        currentFrontScroll -= scroll * zoomSpeed;
+        currentFrontScroll = Mathf.Clamp(currentFrontScroll, zoomMin, zoomMax);
+        frontSpot.transform.localPosition = currentFrontScroll * Vector3.forward;
+
+        RaycastHit hit2;
+        if (Physics.Raycast(transform.position, frontSpot.transform.position - transform.position, out hit2, currentFrontScroll + deltaClip))
+        {
+            if ((hit2.point - transform.position).sqrMagnitude > zoomMin * zoomMin)
+            {
+                frontSpot.transform.position = hit2.point;
+            }
+            else
+            {
+                frontSpot.transform.localPosition = zoomMin * Vector3.forward;
+            }
+            frontSpot.transform.localPosition -= frontSpot.transform.localPosition.normalized * deltaClip * (1.0f - Vector3.Dot(hit2.normal, frontSpot.transform.localPosition.normalized));
+
+        }
+
+        //Gradient
+
+        float visibility2 = Mathf.Clamp01((frontSpot.transform.localPosition.magnitude - minimumPlayerDisplayDistance) / (totalPlayerDisplayDistance - minimumPlayerDisplayDistance));
+
+
+        if (visibility2 < 1 && enableGradientPlayer)
+        {
+            thirdPersonCamera.cullingMask &= ~((1 << 18) + (1 << 9) + (1 << 10)); //Disable the layer
+            thirdPersonCameraPlayer.gameObject.SetActive(true);
+            displayImagePlayer.color = new Color(1, 1, 1, visibility2);
+        }
+        else
+        {
+            thirdPersonCamera.cullingMask |= ((1 << 18) + (1 << 9) + (1 << 10)); //Enable the layer
+            displayImagePlayer.color = new Color(1, 1, 1, 0);
+        }
+    }
+
+    private void SwitchState()
+    {
+        currentState = (currentState + 1) % 3;
+
+        switch (currentState)
+        {
+            case 0:
+                displayImage.gameObject.SetActive(false);
+                displayImagePlayer.gameObject.SetActive(false);
+                displayImageUI.gameObject.SetActive(false);
+                break;
+            case 1:
+                Switch3rdPerson();
+                thirdPersonCamera.transform.parent = backSpot;
+                thirdPersonCamera.transform.localRotation = Quaternion.identity;
+                thirdPersonCameraPlayer.transform.parent = backSpot;
+                thirdPersonCameraPlayer.transform.localRotation = Quaternion.identity;
+                thirdPersonCamera.transform.localPosition = Vector3.zero;
+                thirdPersonCameraPlayer.transform.localPosition = Vector3.zero;
+                break;
+            case 2:
+                Switch3rdPerson();
+                thirdPersonCamera.transform.parent = frontSpot;
+                thirdPersonCamera.transform.localRotation = Quaternion.Euler(0,180,0);
+                thirdPersonCameraPlayer.transform.parent = frontSpot;
+                thirdPersonCameraPlayer.transform.localRotation = Quaternion.Euler(0, 180, 0);
+                thirdPersonCamera.transform.localPosition = Vector3.zero;
+                thirdPersonCameraPlayer.transform.localPosition = Vector3.zero;
+                break;
+        }
+
+
+    }
+
+    private void Switch3rdPerson()
+    {
+        displayImage.gameObject.SetActive(true);
+        displayImageUI.gameObject.SetActive(true);
+        displayImagePlayer.gameObject.SetActive(true);
+        
+    }
+
+}
---
+++ Readme.md
@@ -0,0 +1,88 @@
+# Easy3rdPerson - Quick and free way to allow 3rd person in your VRChat Udon world for Desktop players.
+
+## Overview
+
+* This is a script written in C# using UdonSharp for VRChat, a social virtual reality platform. The script provides a way to toggle between first-person and third-person camera modes and enables various camera settings like zoom, center translate, and clip settings. It also allows the user to set up a player fade effect, where the visibility of the player model fades as the user moves further away from it.
+
+* The script begins by defining several private variables and public settings. The public settings can be modified from the Inspector window in Unity Editor, allowing users to customize the behavior of the script. The private variables, on the other hand, are used internally by the script and should not be modified directly.
+
+* The script contains several functions that are called at various points in its lifecycle. Start() initializes some of the variables and disables the script if the user is in VR mode. Update() is called once per frame and is responsible for updating the position and rotation of the camera based on the user's head tracking data. It also checks if the user has pressed a key to switch camera modes and updates the camera settings accordingly.
+
+* The UpdateRenderTexture() function is used to update the render textures used by the cameras. This function is called once at the beginning of the script and every time the display size of the camera changes. The UpdateThirdRenderTexture(), UpdateFirstRenderTexture(), and UpdateThirdRenderTexturePlayer() functions are helper functions that create new render textures for the cameras and update the textures when necessary.
+
+* The script also contains a number of private variables that are used to keep track of the camera state and various settings. These variables are updated when the user presses the appropriate keys or when the camera is moved.
+
+* Overall, the script provides a way to quickly and easily switch between first-person and third-person camera modes, with a number of customizable settings for both modes. It is designed to work within the VRChat environment, but could potentially be adapted for use in other applications or games.
+
+## Default Controls
+
+* Use the Mouse Wheel Scroll to zoom towards your character in Third Person View (Front or Back).
+* Use X to switch from a view to another (First -> Third Back -> Third Front -> First -> ...).
+* Use Left Control and Mouse Wheel Scroll to translate vertically the center of third person camera rotation.
+
+## Install
+
+To install the script, drag and drop the prefab Easy3rdPerson.prefab in the scene after opening the unity package.
+
+## Parameters
+
+Do not touch the Internal parameters in the inspector!
+
+* switchViewInput: The key input for switching between the 1st and 3rd person view.
+* zoomSpeed: The speed of the zoom function.
+* zoomMin: The minimum zoom value.
+* zoomMax: The maximum zoom value.
+* translateCenterInput: The key input for translating the center of the view.
+* deltaCenterSpeed: The speed of the center translation.
+* maxDeltaCenter: The maximum value for the center translation.
+* minDeltaCenter: The minimum value for the center translation.
+* clipLayers: The layers to clip with.
+* enableDeltaClip: A boolean that enables the clipping feature.
+* deltaClip: The value of the clip offset.
+* enableGradientPlayer: A boolean that enables the player fade feature.
+* minimumPlayerDisplayDistance: The minimum distance the player is displayed on the canvas.
+* totalPlayerDisplayDistance: The total distance the player is displayed on the canvas.
+
+## Known issues
+
+* The prefab may reduces FPS since it works with RenderTextures.
+* Sometimes, the menu may not show properly, just switch back to first person mode, change avatar, move a bit and it should be fine.
+
+## Advanced Information for developers
+
+### Private Variables:
+
+* displayImageTransform: A reference to the RectTransform of the RawImage.
+* displayImage: A reference to the RawImage for the 3rd person view.
+* displayImageUI: A reference to the RawImage for the 1st person view.
+* displayImagePlayer: A reference to the RawImage for the player view.
+* thirdPersonCamera: A reference to the 3rd person camera.
+* thirdPersonCameraPlayer: A reference to the player camera.
+* firstPersonCamera: A reference to the 1st person camera.
+* backSpot: A reference to the Transform for the back position.
+* frontSpot: A reference to the Transform for the front position.
+* loadingWarning: A reference to the loading warning object.
+* firstPersonRenderTexture: The RenderTexture for the 1st person view.
+* thirdPersonRenderTexture: The RenderTexture for the 3rd person view.
+* thirdPersonRenderTexturePlayer: The RenderTexture for the player view.
+* displaySize: The size of the display.
+* currentState: The current state of the view (0 = 1st person, 1 = back, 2 = front).
+* currentFrontScroll: The current value of the front scroll.
+* currentBackScroll: The current value of the back scroll.
+* currentDeltaCenter: The current value of the center translation.
+
+### Private Methods:
+
+* Start(): Initializes the script by checking if the user is in VR and updating the render texture.
+* UpdateRenderTexture(): Updates the render texture for the 3rd person view and the player view.
+* UpdateThirdRenderTexture(): Updates the render texture for the 3rd person view.
+* UpdateFirstRenderTexture(): Updates the render texture for the 1st person view.
+* UpdateThirdRenderTexturePlayer(): Updates the render texture for the player view.
+* Update(): Updates the view and checks for user input.
+* UpdateScroll() : Updates the current scroll view and gradient of the player.
+* UpdateScrollBack(float scroll) : Helper method for Update Scroll for the back view.
+* UpdateScrollFront(float scroll) : Helper method for Update Scroll for the front view.
+* SwitchState() : Switch the view between first, back and front.
+* Switch3rdPerson() : Helper method for SwitchState() for regrouping common behaviours between back and front.
+
+Note: This advanced section assumes that the user has a basic understanding of Unity and scripting.
